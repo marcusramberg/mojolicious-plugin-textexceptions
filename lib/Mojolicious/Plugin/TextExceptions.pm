afbb9c61c21c08d0d5de5d0aa85baf5c234c6238
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-03-06 17:06:32 +0800

    No need to support undef($ua_re)

--- lib/Mojolicious/Plugin/TextExceptions.pm
+++ lib/Mojolicious/Plugin/TextExceptions.pm
@@ -5,7 +5,7 @@ our $VERSION = '0.1';
 
 sub register {
   my ($self, $app, $config) = @_;
-  my $ua_re = exists $config->{ua_re} ? $config->{ua_re} : qr{^(Mojolicious|curl|Wget)};
+  my $ua_re = $config->{ua_re} || qr{^(Mojolicious|curl|Wget)};
 
   $app->hook(
     before_render => sub {
@@ -14,7 +14,7 @@ sub register {
       return unless my $template = $args->{template};
       return unless $template eq 'exception';
 
-      if ($ua_re and $c->req->headers->user_agent =~ $ua_re) {
+      if ($c->req->headers->user_agent =~ $ua_re) {
         @$args{qw(text format)} = ($c->stash->{exception}, 'txt');
       }
     }
